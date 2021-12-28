8a4e37a77187f8f8b7f2ea2de90eedb8fae287ca
maik-nack <mish_1998@mail.ru>
2021-12-29 00:10:50 +0300

    Added solution code

---
+++ app.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+from itertools import accumulate, islice
+from math import isclose
+from typing import List
+
+from interval import Interval
+
+
+def read_intervals(filepath: str) -> List[Interval]:
+    with open(filepath, 'r') as file:
+        return [Interval(mid, rad) for (mid, rad) in map(lambda line: map(float, line.split()), file.readlines())]
+
+
+def generate_subintervals(intervals: List[Interval]) -> List[Interval]:
+    bounds = [[interval.min, interval.max] for interval in intervals]
+    points = sorted(set(bound for interval in bounds for bound in interval))
+    i = 0
+    for j, point in enumerate(islice(points, 1, len(points))):
+        if not isclose(point, points[i]):
+            i += 1
+            points[i] = points[j + 1]
+    return [Interval((points[idx - 1] + points[idx]) / 2, abs(points[idx - 1] - points[idx]) / 2)
+            for idx in range(1, i + 1)]
+
+
+def calculate_frequencies(intervals: List[Interval], subintervals: List[Interval]) -> List[int]:
+    subintervals_mids = [subinterval.mid for subinterval in subintervals]
+    return [sum(1 for interval in intervals if subinterval_mid in interval) for subinterval_mid in subintervals_mids]
+
+
+def calculate_median_by_frequencies(subintervals: List[Interval], frequencies: List[int]) -> Interval:
+    frequencies_cumsum = list(accumulate(frequencies))
+    m = frequencies_cumsum[-1] / 2.
+    idx, _ = max(enumerate(frequencies_cumsum), key=lambda f: f[1] >= m)
+    if isclose(frequencies_cumsum[idx], m):
+        return (subintervals[idx] + subintervals[idx + 1]) / 2
+    return subintervals[idx]
+
+
+def calculate_median_by_distances_to_intervals(intervals: List[Interval], subintervals: List[Interval]) -> Interval:
+    distances_sum = [sum(subinterval.distance_to(interval) for interval in intervals) for subinterval in subintervals]
+    idx, _ = min(enumerate(distances_sum), key=lambda d: d[1])
+    return subintervals[idx]
+
+
+def calculate_median_by_distances_and_frequencies(subintervals: List[Interval], frequencies: List[int]) -> Interval:
+    distances_sum = [sum(subinterval_i.distance_to(subinterval_j) * frequencies[j]
+                         for j, subinterval_j in enumerate(subintervals) if j != i)
+                     for i, subinterval_i in enumerate(subintervals)]
+    idx, _ = min(enumerate(distances_sum), key=lambda d: d[1])
+    return subintervals[idx]
+
+
+def main():
+    intervals = read_intervals('intervals')
+    subintervals = generate_subintervals(intervals)
+    frequencies = calculate_frequencies(intervals, subintervals)
+    frequency_median = calculate_median_by_frequencies(subintervals, frequencies)
+    distance_median = calculate_median_by_distances_to_intervals(intervals, subintervals)
+    distance_frequency_median = calculate_median_by_distances_and_frequencies(subintervals, frequencies)
+
+    print(frequency_median)
+    print(distance_median)
+    print(distance_frequency_median)
+
+    df = pd.DataFrame(columns=['interval', 'e', 'y', 'type'])
+    for i, interval in enumerate(intervals):
+        df = df.append(dict(interval=interval.mid, e=interval.rad, y=i + 1, type='interval'), ignore_index=True)
+    for subinterval in subintervals:
+        df = df.append(dict(interval=subinterval.mid, e=subinterval.rad, y=0, type='subinterval'), ignore_index=True)
+    df = df.append(dict(interval=frequency_median.mid, e=frequency_median.rad, y=-1, type='frequency_median'),
+                   ignore_index=True)
+    df = df.append(dict(interval=distance_median.mid, e=distance_median.rad, y=-2, type='distance_median'),
+                   ignore_index=True)
+    df = df.append(dict(interval=distance_frequency_median.mid, e=distance_frequency_median.rad, y=-3,
+                        type='distance_frequency_median'), ignore_index=True)
+    fig = px.scatter(df, x='interval', y='y', color='type', error_x='e')
+    intervals_count = len(intervals)
+    fig.add_trace(go.Scatter(x=[subintervals[0].min, subintervals[0].min], y=[-3, intervals_count], mode='lines',
+                             line=dict(color='black', width=1, dash='dash'), showlegend=False))
+    for subinterval in subintervals:
+        fig.add_trace(go.Scatter(x=[subinterval.max, subinterval.max], y=[-3, intervals_count], mode='lines',
+                                 line=dict(color='black', width=1, dash='dash'), showlegend=False))
+    fig.update_layout(xaxis=dict(dtick=1, title=''), yaxis=dict(showticklabels=False, visible=False))
+    fig.write_html('intervals.html')
+
+
+if __name__ == '__main__':
+    main()
---
+++ interval.py
@@ -0,0 +1,74 @@
+from math import isclose
+
+
+class Interval:
+    mid: float
+    rad: float
+
+    def __init__(self, mid: float, rad: float):
+        self._mid = mid
+        self._rad = rad
+
+    @property
+    def mid(self) -> float:
+        return self._mid
+
+    @property
+    def rad(self) -> float:
+        return self._rad
+
+    @property
+    def min(self) -> float:
+        return self._mid - self._rad
+
+    @property
+    def max(self) -> float:
+        return self._mid + self._rad
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Interval):
+            return bool(isclose(self._mid, other._mid) and isclose(self._rad, other._rad))
+        raise TypeError(f"unsupported operand type(s) for ==: 'Interval' and '{type(other).__name__}'")
+
+    def __add__(self, other) -> 'Interval':
+        if isinstance(other, Interval):
+            return Interval(self._mid + other._mid, self._rad + other._rad)
+        raise TypeError(f"unsupported operand type(s) for +: 'Interval' and '{type(other).__name__}'")
+
+    def __sub__(self, other) -> 'Interval':
+        if isinstance(other, Interval):
+            return Interval(self._mid - other._mid, self._rad + other._rad)
+        raise TypeError(f"unsupported operand type(s) for +: 'Interval' and '{type(other).__name__}'")
+
+    def __mul__(self, other) -> 'Interval':
+        if isinstance(other, float) or isinstance(other, int):
+            return Interval(self._mid * other, self._rad * other)
+        elif isinstance(other, Interval):
+            minmin = self.min * other.min
+            minmax = self.min * other.max
+            maxmin = self.max * other.min
+            maxmax = self.max * other.max
+            min_ = min(minmin, minmax, maxmin, maxmax)
+            max_ = max(minmin, minmax, maxmin, maxmax)
+            return Interval((min_ + max_) / 2., (max_ - min_) / 2.)
+        raise TypeError(f"unsupported operand type(s) for *: 'Interval' and '{type(other).__name__}'")
+
+    def __truediv__(self, other) -> 'Interval':
+        if isinstance(other, float) or isinstance(other, int):
+            return Interval(self._mid / other, self._rad / other)
+        elif isinstance(other, Interval):
+            min_ = 1. / other.max
+            max_ = 1. / other.min
+            return self * Interval((min_ + max_) / 2., (max_ - min_) / 2.)
+        raise TypeError(f"unsupported operand type(s) for /: 'Interval' and '{type(other).__name__}'")
+
+    def __contains__(self, item) -> bool:
+        if isinstance(item, float) or isinstance(item, int):
+            return self.min <= item <= self.max
+        raise TypeError(f"'in <Interval>' requires float or int as left operand, not {type(item).__name__}")
+
+    def __repr__(self):
+        return '[{:g}, {:g}], {:g} ± {:g}'.format(self.min, self.max, self._mid, self._rad)
+
+    def distance_to(self, other: 'Interval') -> float:
+        return max(abs(self.min - other.min), abs(self.max - other.max))
